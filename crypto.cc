5dbca84db7896d9a7f79399c73bb8fd0d9a462f4
Gabriel Fernandes <gabrielfernndss@gmail.com>
2018-03-29 07:37:03 -0300

    fixed issue when input and output file were the same and set k to 0 for security reasons

--- crypto.cc
+++ crypto.cc
@@ -14,7 +14,7 @@ int main(int argc, char** argv)
     fstream key(argv[1]);
     fstream input(argv[2]);
     fstream output;
-    if (argc < 4)
+    if (argc < 4 || string(argv[2]) == argv[3])
         output = fstream(argv[2]);
     else
         output = fstream(argv[3], output.out);
@@ -25,7 +25,7 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    char k, b = 0;
+    char k = 0, b;
     while (input.get(b))
     {
         if (!key.get(k))
